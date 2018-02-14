4bf574b7d6f6a4cb7e6d7083c0686b1ef4466828
Canaan Seaton <cseaton@bastiansolutions.com>
2018-02-14 13:07:47 -0500

    added encapsulation build and parse methods

--- src/enip/encapsulation/encapsulation.spec.js
+++ src/enip/encapsulation/encapsulation.spec.js
@@ -2,7 +2,7 @@ const encapsulation = require("./index");
 
 describe("ENIP Encapsulation", () => {
     describe("Command Validator", () => {
-        const { ValidateCommand } = encapsulation;
+        const { validateCommand } = encapsulation;
         const {
             RegisterSession,
             UnregisterSession,
@@ -11,18 +11,18 @@ describe("ENIP Encapsulation", () => {
         } = encapsulation.commands;
 
         it("Rejects Invalid Commands", () => {
-            expect(ValidateCommand(0x99)).toBeFalsy();
-            expect(ValidateCommand("hello")).toBeFalsy();
-            expect(ValidateCommand(0x02)).toBeFalsy();
+            expect(validateCommand(0x99)).toBeFalsy();
+            expect(validateCommand("hello")).toBeFalsy();
+            expect(validateCommand(0x02)).toBeFalsy();
         });
 
         it("Accepts Proper Commands", () => {
-            expect(ValidateCommand(0x66)).toBeTruthy();
-            expect(ValidateCommand(102)).toBeTruthy();
-            expect(ValidateCommand(RegisterSession)).toBeTruthy();
-            expect(ValidateCommand(UnregisterSession)).toBeTruthy();
-            expect(ValidateCommand(SendRRData)).toBeTruthy();
-            expect(ValidateCommand(SendUnitData)).toBeTruthy();
+            expect(validateCommand(0x66)).toBeTruthy();
+            expect(validateCommand(102)).toBeTruthy();
+            expect(validateCommand(RegisterSession)).toBeTruthy();
+            expect(validateCommand(UnregisterSession)).toBeTruthy();
+            expect(validateCommand(SendRRData)).toBeTruthy();
+            expect(validateCommand(SendUnitData)).toBeTruthy();
         });
     });
 });
--- src/enip/encapsulation/index.js
+++ src/enip/encapsulation/index.js
@@ -1,8 +1,9 @@
 const { Socket } = require("net");
 
-const Header = {};
+const header = {};
 const commands = {
-    ListServices: 0x01,
+    NOP: 0x00,
+    ListServices: 0x04,
     ListIdentity: 0x63,
     ListInterfaces: 0x64,
     RegisterSession: 0x65, // Begin Session Command
@@ -13,15 +14,128 @@ const commands = {
     Cancel: 0x73
 };
 
-const ValidateCommand = cmd => {
+/**
+ * Parses Encapulation Status Code to Human Readable Error Message.
+ *
+ * @param {number} status - Status Code
+ * @returns {string} Human Readable Error Message
+ */
+const parseStatus = status => {
+    if (typeof status !== "Number")
+        throw new Error("ERROR: parseStatus accepts type <Number> only!");
+
+    switch (status) {
+        case 0x00:
+            return "SUCCESS";
+        case 0x01:
+            return "FAIL: Sender issued an invalid ecapsulation command.";
+        case 0x02:
+            return "FAIL: Insufficient memory resources to handle command.";
+        case 0x03:
+            return "FAIL: Poorly formed or incorrect data in encapsulation packet.";
+        case 0x64:
+            return "FAIL: Originator used an invalid session handle.";
+        case 0x65:
+            return "FAIL: Target received a message of invalid length.";
+        case 0x69:
+            return "FAIL: Unsupported encapsulation protocol revision.";
+        default:
+            return `FAIL: General failure <${status}> occured.`;
+    }
+};
+
+/**
+ * Checks if Command is a Valid Encapsulation Command
+ *
+ * @param {Number} ecapsulation command
+ * @returns {boolean} test result
+ */
+const validateCommand = cmd => {
     for (let key of Object.keys(commands)) {
         if (cmd === commands[key]) return true;
     }
     return false;
 };
 
-Header.Build = (cmd, session = 0x00) => {};
+/**
+ * Builds an ENIP Encapsolated Packet
+ *
+ * @param {number} cmd - Command to Send
+ * @param {number} [session=0x00] - Session ID
+ * @param {Buffer|Array} [data=[]] - Data to Send
+ * @returns {Buffer} - Generated Buffer to be Sent to Target
+ */
+header.build = (cmd, session = 0x00, data = []) => {
+    // Validate requested command
+    if (!ValidateCommand(cmd)) throw new Error("ERROR: Invalid Encapsulation Command!");
 
-module.exports.Header = Header;
+    const buf = Buffer.from(data);
+    const send = {
+        cmd: cmd,
+        length: buf.length,
+        session: session,
+        status: 0x00,
+        context: Buffer.alloc(8, 0x00),
+        options: 0x00,
+        data: buf
+    };
+
+    // Initialize header buffer to appropriate length
+    let header = Buffer.alloc(24 + send.length);
+
+    // Build header from encapsulation data
+    header.writeInt16LE(send.cmd, 0);
+    header.writeInt16LE(send.length, 2);
+    header.writeInt32LE(send.seesion, 4);
+    header.writeInt32LE(send.status, 8);
+    send.context.copy(header, 12);
+    header.writeInt32LE(send.options, 20);
+    send.data.copy(header, 24);
+
+    return header;
+};
+
+/**
+ * @typedef EncapsulationData
+ * @type {Object}
+ * @property {number} command - Ecapsulation Command
+ * @property {number} length - Length of Encapsulated Data
+ * @property {number} session - Session ID
+ * @property {number} statusCode - Status Code
+ * @property {string} status - Status Code String Interpretation
+ * @property {number} options - Options (Typically 0x00)
+ * @property {Buffer} data - Encapsulated Data Buffer
+ */
+
+/**
+ * Parses an Encapsulated Packet Received from ENIP Target
+ *
+ * @param {Buffer} buf - Incoming Encapsulated Buffer from Target
+ * @returns {EncapsulationData} - Parsed Encapsulation Data Object
+ */
+header.parse = buf => {
+    if (typeof buf !== "Buffer") throw new Error("ERROR: header.parse accepts type <Buffer> only!");
+
+    const received = {
+        command: buf.readInt16LE(0),
+        length: buf.readInt16LE(2),
+        session: buf.readInt32LE(4),
+        statusCode: buf.readInt32LE(8),
+        status: null,
+        options: buf.readInt32LE(20),
+        data: null
+    };
+
+    // Get Returned Encapsulated Data
+    let dataBuffer = Buffer.alloc(received.length);
+    buf.copy(dataBuffer, 24);
+
+    received.data = dataBuffer;
+    received.status = parseStatus(received.statusCode);
+
+    return received;
+};
+
+module.exports.header = header;
 module.exports.commands = commands;
-module.exports.ValidateCommand = ValidateCommand;
+module.exports.validateCommand = validateCommand;
