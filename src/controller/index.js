93899f4400f021c44f3f0a8679eec0d49801d503
Canaan Seaton <cseaton@bastiansolutions.com>
2018-02-14 22:04:37 -0500

    Removed unused dependencies from project

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,6 +1,6 @@
-const { Socket } = require("net");
+const net = require("net");
+const { Socket } = net;
 const { EIP_PORT } = require("../config");
-const validator = require("validator");
 
 class Controller {
     constructor(IP_ADDR) {
@@ -8,7 +8,7 @@ class Controller {
             throw new Error("ERROR: Controller <class> requires IP_ADDR <string>!!!");
         }
 
-        if (!validator.isIP(IP_ADDR)) {
+        if (net.isIPv4(IP_ADDR)) {
             throw new Error("ERROR: Invalid IP_ADDR <string> passed to Controller <class>");
         }
 
