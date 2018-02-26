71581a33fb17fed6516b23bc542356578725c8b6
Canaan Seaton <cseaton@bastiansolutions.com>
2018-02-26 12:38:30 -0500

    fixed bugs in message router and added tests for message router utilities

--- src/enip/cip/message-router/index.js
+++ src/enip/cip/message-router/index.js
@@ -1,5 +1,3 @@
-const MR = {}; // message router
-
 /**
  * Builds a Message Router Request Buffer
  *
@@ -8,20 +6,19 @@ const MR = {}; // message router
  * @param {Buffer} data - Service Specific Data to be Sent
  * @returns {Buffer} Message Router Request Buffer
  */
-MR.build = (service, path, data) => {
+build = (service, path, data) => {
     const pathBuf = Buffer.from(path);
     const dataBuf = Buffer.from(data);
 
-    const buf = Buffer.alloc(2 + pathBuf.length + dataBuf.length);
+    const pathLen =
+        pathBuf.length % 2 === 1 ? Math.trunc(pathBuf.length / 2) + 1 : Math.trunc(pathBuf.length);
+    const buf = Buffer.alloc(2 + pathLen * 2 + dataBuf.length);
 
-    buf.writeInt8(service); // Write Service Code to Buffer <USINT>
-    buf.writeInt8(
-        // Write Length of EPATH (16 bit word length)
-        pathBuf.length % 2 === 1 ? Math.round(pathBuf.length / 2) + 1 : Math.round(pathBuf.length)
-    );
+    buf.writeInt8(service, 0); // Write Service Code to Buffer <USINT>
+    buf.writeInt8(pathLen, 1); // Write Length of EPATH (16 bit word length)
 
     pathBuf.copy(buf, 2); // Write EPATH to Buffer
-    dataBuf.copy(buf, 2 + pathBuf.length); // Write Service Data to Buffer
+    dataBuf.copy(buf, 2 + pathLen * 2); // Write Service Data to Buffer
 
     return buf;
 };
@@ -31,7 +28,6 @@ MR.build = (service, path, data) => {
  * @type {Object}
  * @property {number} service - Reply Service Code
  * @property {number} generalStatusCode - General Status Code (Vol 1 - Appendix B)
- * @property {string} generalStatus - General Status String Interpretation
  * @property {number} extendedStatusLength - Length of Extended Status (In 16-bit Words)
  * @property {Array} extendedStatus - Extended Status
  * @property {Buffer} data - Status Code
@@ -43,17 +39,14 @@ MR.build = (service, path, data) => {
  * @param {Buffer} buf - Message Router Request Buffer
  * @returns {MessageRouter} Decoded Message Router Object
  */
-MR.parse = buf => {
+parse = buf => {
     let MessageRouter = {
         service: buf.readInt8(0),
         generalStatusCode: buf.readInt8(2),
-        generalStatus: null,
         extendedStatusLength: buf.readInt8(3),
         extendedStatus: null,
         data: null
     };
-
-    // TODO: Add General Status Code Parser Function 
 
     // Build Extended Status Array
     let arr = [];
@@ -61,18 +54,18 @@ MR.parse = buf => {
         arr.push(buf.readInt16LE(i * 2 + 4));
     }
     MessageRouter.extendedStatus = arr;
-    
+
     // Get Starting Point of Message Router Data
-    const dataStart = MessageRouter.extendedStatusLength * 2 - 4;
+    const dataStart = MessageRouter.extendedStatusLength * 2 + 4;
 
     // Initialize Message Router Data Buffer
     let data = Buffer.alloc(buf.length - dataStart);
-    
+
     // Copy Data to Message Router Data Buffer
     buf.copy(data, 0, dataStart);
     MessageRouter.data = data;
 
-    return MessageRouter
+    return MessageRouter;
 };
 
-module.exports = { MR };
+module.exports = { build, parse };
