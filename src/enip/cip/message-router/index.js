57a774771b710dccb9a93d6197553f25a690149b
Canaan Seaton <cseaton@bastiansolutions.com>
2018-02-26 00:19:48 -0500

    Finished Message Router Parser, Need to add Tests

--- src/enip/cip/message-router/index.js
+++ src/enip/cip/message-router/index.js
@@ -15,7 +15,8 @@ MR.build = (service, path, data) => {
     const buf = Buffer.alloc(2 + pathBuf.length + dataBuf.length);
 
     buf.writeInt8(service); // Write Service Code to Buffer <USINT>
-    buf.writeInt8( // Write Length of EPATH (16 bit word length)
+    buf.writeInt8(
+        // Write Length of EPATH (16 bit word length)
         pathBuf.length % 2 === 1 ? Math.round(pathBuf.length / 2) + 1 : Math.round(pathBuf.length)
     );
 
@@ -25,5 +26,53 @@ MR.build = (service, path, data) => {
     return buf;
 };
 
+/**
+ * @typedef MessageRouter
+ * @type {Object}
+ * @property {number} service - Reply Service Code
+ * @property {number} generalStatusCode - General Status Code (Vol 1 - Appendix B)
+ * @property {string} generalStatus - General Status String Interpretation
+ * @property {number} extendedStatusLength - Length of Extended Status (In 16-bit Words)
+ * @property {Array} extendedStatus - Extended Status
+ * @property {Buffer} data - Status Code
+ */
+
+/**
+ * Parses a Message Router Request Buffer
+ *
+ * @param {Buffer} buf - Message Router Request Buffer
+ * @returns {MessageRouter} Decoded Message Router Object
+ */
+MR.parse = buf => {
+    let MessageRouter = {
+        service: buf.readInt8(0),
+        generalStatusCode: buf.readInt8(2),
+        generalStatus: null,
+        extendedStatusLength: buf.readInt8(3),
+        extendedStatus: null,
+        data: null
+    };
+
+    // TODO: Add General Status Code Parser Function 
+
+    // Build Extended Status Array
+    let arr = [];
+    for (let i = 0; i < MessageRouter.extendedStatusLength; i++) {
+        arr.push(buf.readInt16LE(i * 2 + 4));
+    }
+    MessageRouter.extendedStatus = arr;
+    
+    // Get Starting Point of Message Router Data
+    const dataStart = MessageRouter.extendedStatusLength * 2 - 4;
+
+    // Initialize Message Router Data Buffer
+    let data = Buffer.alloc(buf.length - dataStart);
+    
+    // Copy Data to Message Router Data Buffer
+    buf.copy(data, 0, dataStart);
+    MessageRouter.data = data;
+
+    return MessageRouter
+};
 
 module.exports = { MR };
