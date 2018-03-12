8e9d20d9688c6366a2b992c27ac4c00bbf83cea6
Canaan Seaton <cmseaton42@gmail.com>
2018-03-12 00:21:03 -0400

    Implemented Task Queue Utility, Adding Tests

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -22,4 +22,108 @@ const promiseTimeout = (promise, ms, error = new Error("ASYNC Function Call Time
  */
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-module.exports = { promiseTimeout, delay };
+/**
+ * A simple Job Scheduler/ Task Runner class
+ *
+ * @param {function} compare_func - Compares Priority Objects
+ * @class JobQueue
+ */
+class TaskQueue {
+    constructor(compare_func) {
+        this.tasks = [];
+        this.taskRunning = false;
+
+        if (typeof compare_func !== "function")
+            throw new Error(
+                `JobQueue Comparison Function must be of Type <function> instead got ${typeof compare_func}`
+            );
+
+        this.compare = compare_func;
+    }
+
+    schedule(task, args, priority_obj) {
+        if (typeof task !== "function")
+            throw new Error(`Scheduler Task must be of Type <function> instead got ${typeof task}`);
+        if (!Array.isArray(args))
+            throw new Error(`Scheduler args must be of Type <Array> instead got ${typeof args}`);
+        if (typeof priority_obj !== "object")
+            throw new Error(
+                `Scheduler Task must be of Type <Object> instead got ${typeof priority_obj}`
+            );
+
+        // return Promise to Caller
+        return new Promise((resolve, reject) => {
+            // Push Task to Queue
+            this.tasks.push({ task, args, priority_obj, resolve, reject });
+            this._next();
+        });
+    }
+
+    _getParent(index) {
+        return Math.trunc((index - 1) / 2);
+    }
+
+    _getLNode(index) {
+        return index * 2 + 1;
+    }
+
+    _getRNode(index) {
+        return index * 2 + 2;
+    }
+
+    _reorder(index) {
+        const { compare } = this;
+        const size = this.tasks.length;
+
+        const l = this._getLNode(index);
+        const r = this._getRNode(index);
+
+        let swap = index;
+
+        if (l < size && compare(this.tasks[l].priority_obj, this.tasks[swap].priority_obj))
+            swap = l;
+        if (r < size && compare(this.tasks[r].priority_obj, this.tasks[swap].priority_obj))
+            swap = r;
+
+        if (swap !== index) { 
+            const temp = this.tasks[index];
+            this.tasks[index] = this.tasks[swap];
+            this.tasks[swap] = temp;
+            this._reorder(swap);
+        }
+    }
+
+    _orderQueue() {
+        const size = this.tasks.length;
+        const start = Math.trunc((size - 2) / 2);
+
+        for (let i = start; i >= 0; i--) {
+            this._reorder(i);
+        }
+    }
+
+    _runTask() {
+        this._orderQueue();
+
+        const job = this.tasks.shift();
+        const { task, args, resolve, reject } = job;
+
+        this.taskRunning = true;
+
+        task(...args)
+            .then(resolve)
+            .catch(reject)
+            .finally(() => {
+                this.taskRunning = false;
+                this._next();
+            });
+    }
+
+    _next() {
+        if (this.tasks.length !== 0 && this.tasks.taskRunning === false) {
+            this._runTask();
+        }
+    }
+}
+
+module.exports = { promiseTimeout, delay, TaskQueue };
--- src/utilities/utilities.spec.js
+++ src/utilities/utilities.spec.js
@@ -1,4 +1,4 @@
-const { promiseTimeout, delay } = require("./index");
+const { promiseTimeout, delay, TaskQueue } = require("./index");
 
 describe("Utilites", () => {
     describe("Promise Timeout Utility", () => {
@@ -15,7 +15,7 @@ describe("Utilites", () => {
                     "error"
                 );
             };
-            
+
             await expect(fn(200)).rejects.toMatch("error");
             await expect(fn(110)).rejects.toMatch("error");
             await expect(fn(90)).resolves.toBeUndefined();
@@ -39,9 +39,66 @@ describe("Utilites", () => {
             };
 
             await expect(fn(200)).rejects.toMatch("error");
-            await expect(fn(101)).rejects.toMatch("error");
-            await expect(fn(99)).resolves.toBeUndefined();
+            await expect(fn(110)).rejects.toMatch("error");
+            await expect(fn(90)).resolves.toBeUndefined();
             await expect(fn(50)).resolves.toBeUndefined();
+        });
+    });
+
+    describe("Task Queue Class", () => {
+        describe("New Instance", () => {
+            it("Rejects Improper Constructor Arguments", () => {
+                const fn = arg => () => new TaskQueue(arg);
+
+                expect(fn()).toThrow();
+                expect(fn("hello")).toThrow();
+                expect(fn({ a: 6 })).toThrow();
+                expect(fn(() => console.log("hello"))).not.toThrow();
+            });
+        });
+
+        describe("Private Methods", () => {
+            let compare;
+            let arr;
+            let q;
+
+            beforeEach(() => {
+                arr = [];
+                compare = (obj1, obj2) => obj1.value >= obj2.value;
+
+                for (let i = 0; i < 15; i++) {
+                    arr.push({
+                        task: delay,
+                        args: [100 * i],
+                        priority_obj: { value: i }
+                    });
+                }
+
+                q = new TaskQueue(compare);
+                q.tasks = arr;
+            });
+
+            it("Primitive Heap Methods Generate Correct Values", () => {
+                expect(q._getParent(5)).toEqual(2);
+                expect(q._getParent(6)).toEqual(2);
+
+                expect(q._getLNode(5)).toEqual(11);
+                expect(q._getLNode(25)).toEqual(51);
+
+                expect(q._getRNode(5)).toEqual(12);
+                expect(q._getRNode(25)).toEqual(52);
+            });
+
+            it("Reorder Method Produces Correct Output", () => {
+                q._reorder(0);
+                expect(q.tasks).toMatchSnapshot();
+            });
+
+            it("Build Heap Works Correctly", () => {
+                q._orderQueue();
+                
+                expect(q.tasks).toMatchSnapshot();
+            });
         });
     });
 });
