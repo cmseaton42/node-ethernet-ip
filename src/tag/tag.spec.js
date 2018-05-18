495de2a1f88d8e47085aff8616fb4a79142d7250
Patrick McDonagh <patrickjmcd@gmail.com>
2018-05-18 15:22:22 -0500

    Allow 1-character Tag Names (#18)
    * Fix tag regex to allow 1-character tag names
    
    Fix for cmseaton42/node-ethernet-ip#17
    
    * adds a test for 1-characters numeric tag names
    
    * Increments version

--- src/tag/tag.spec.js
+++ src/tag/tag.spec.js
@@ -26,6 +26,9 @@ describe("Tag Class", () => {
             expect(fn(undefined)).toBeFalsy();
             expect(fn(`hello${311}`)).toBeTruthy();
             expect(fn("hello.how3")).toBeTruthy();
+            expect(fn("randy.julian.bubbles")).toBeTruthy();
+            expect(fn("a.b.c")).toBeTruthy();
+            expect(fn("1.1.1")).toBeFalsy();
             expect(fn({ prop: "value" })).toBeFalsy();
             expect(fn("fffffffffffffffffffffffffffffffffffffffff")).toBeFalsy();
             expect(fn("ffffffffffffffffffffffffffffffffffffffff")).toBeTruthy();
