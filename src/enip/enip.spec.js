9b305b04e542c2cddeb751e8deb92d1c9abf82b5
BMh <bmartens@hilscher.com>
2018-10-29 15:04:28 +0100

    add tests for enip connection attributes

--- src/enip/enip.spec.js
+++ src/enip/enip.spec.js
@@ -29,5 +29,50 @@ describe("ENIP Class", () => {
             enip.state.session.id = 23455;
             expect(enip.session_id).toBe(23455);
         });
+        
+        it("establishing_conn", () => {
+            const enip = new ENIP();
+            expect(enip.establishing_conn).toBe(false);
+
+            enip.state.connection.establishing = true;
+            expect(enip.establishing_conn).toBe(true);
+
+            enip.establishing_conn = false;
+            expect(enip.state.connection.establishing).toBe(false);
+        });
+
+        
+        it("established_conn", () => {
+            const enip = new ENIP();
+            expect(enip.established_conn).toBe(false);
+
+            enip.state.connection.established = true;
+            expect(enip.established_conn).toBe(true);
+
+            enip.established_conn = false;
+            expect(enip.state.connection.established).toBe(false);
+        });
+        
+        it("id_conn", () => {
+            const enip = new ENIP();
+            expect(enip.id_conn).toBe(null);
+
+            enip.state.connection.id = 0x1337;
+            expect(enip.id_conn).toBe(0x1337);
+
+            enip.id_conn = 0x00;
+            expect(enip.state.connection.id).toBe(0x00);
+        });
+
+        it("seq_conn", () => {
+            const enip = new ENIP();
+            expect(enip.seq_conn).toBe(0x00);
+
+            enip.state.connection.seq_num = 0x01;
+            expect(enip.seq_conn).toBe(0x01);
+
+            enip.seq_conn = 0x02;
+            expect(enip.state.connection.seq_num).toBe(0x02);
+        });
     });
 });
