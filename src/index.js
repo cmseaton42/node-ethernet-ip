b6e28dae583258a51b9c12740e8d850b90d77d78
BMh <lerux@protonmail.com>
2019-02-25 13:52:08 +0100

    Remove local test

--- src/index.js
+++ src/index.js
@@ -4,18 +4,4 @@ const TagGroup = require("./tag-group");
 const EthernetIP = require("./enip");
 const util = require("./utilities");
 
-/*const mynip = new EthernetIP.ENIP();
-mynip.listServices("192.168.1.11").then(data => {
-    if(data) {
-        console.log(data);
-    }
-});*/
-util.discoverProm()
-    .then((ret) =>{
-        console.log(ret);
-    })
-    .catch((err) =>{
-        console.log(err);
-    });
-
 module.exports = { Controller, Tag, TagGroup, EthernetIP, util };
